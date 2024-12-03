4d52c232540276a701914678c3fc49f67d3b3f93
Lyttr <jgy2001@126.com>
2024-12-02 16:28:24 -0800

    add tfidf model

--- GaoyuanJi/models.py
+++ GaoyuanJi/models.py
@@ -3,11 +3,22 @@ from sklearn.linear_model import LinearRegression
 import pandas as pd
 import torch
 import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
 def signleFeatureRegression(XTrain,YTrain,XTest,YTest):
     
-    model = LinearRegression(
-             
-         
-    )
+    model = LinearRegression()
     model.fit(XTrain, YTrain)
     return model.predict(XTest)
+
+def tfidfRegression(documents,labels,max_features):
+    vectorizer = TfidfVectorizer(max_features=max_features, 
+    stop_words='english', 
+    )
+
+    
+    tfidfMatrix = vectorizer.fit_transform(documents)
+    XTrain, XTest, YTrain, YTest = train_test_split(tfidfMatrix, labels, test_size=0.1, random_state=42)
+    model = LinearRegression()
+    model.fit(XTrain, YTrain)
+    return model.predict(XTest),YTest
--- GaoyuanJi/run.py
+++ GaoyuanJi/run.py
@@ -5,6 +5,7 @@ import pandas as pd
 import torch
 import numpy as np
 import math
+from sklearn.model_selection import train_test_split
 views=dataPreProcessing.dataLoader('US','views').to_list()
 likes=dataPreProcessing.dataLoader('US','likes').to_list()
 feature=[]
@@ -16,10 +17,7 @@ for i in likes:
         entry.append(0)
     feature.append(entry)
 viewsLog=[math.log(i) for i in views]
-XTrain=feature[:(int)(len(feature)*0.9)]
-XTest=feature[(int)(len(feature)*0.9):]
-YTrain=viewsLog[:(int)(len(feature)*0.9)]
-YTest=viewsLog[(int)(len(feature)*0.9):]
+XTrain, XTest, YTrain, YTest = train_test_split(feature, viewsLog, test_size=0.1, random_state=42)
 
 prediction=models.signleFeatureRegression(XTrain,YTrain,XTest,YTest)
 print('SIGNLE FEATURE : LIKES ',eval.evaluate_model(YTest,prediction,'regression',['mse','mae','r2']))
@@ -34,10 +32,7 @@ for i in comments:
         entry.append(0)
     feature.append(entry)
 
-XTrain=feature[:(int)(len(feature)*0.9)]
-XTest=feature[(int)(len(feature)*0.9):]
-YTrain=viewsLog[:(int)(len(feature)*0.9)]
-YTest=viewsLog[(int)(len(feature)*0.9):]
+XTrain, XTest, YTrain, YTest = train_test_split(feature, viewsLog, test_size=0.1, random_state=42)
 prediction=models.signleFeatureRegression(XTrain,YTrain,XTest,YTest)
 print('SIGNLE FEATURE : COMMENTS ',eval.evaluate_model(YTest,prediction,'regression',['mse','mae','r2']))
 ###########################################################
@@ -51,9 +46,18 @@ for i in dislikes:
         entry.append(0)
     feature.append(entry)
 
-XTrain=feature[:(int)(len(feature)*0.9)]
-XTest=feature[(int)(len(feature)*0.9):]
-YTrain=viewsLog[:(int)(len(feature)*0.9)]
-YTest=viewsLog[(int)(len(feature)*0.9):]
+XTrain, XTest, YTrain, YTest = train_test_split(feature, viewsLog, test_size=0.1, random_state=42)
 prediction=models.signleFeatureRegression(XTrain,YTrain,XTest,YTest)
 print('SIGNLE FEATURE : DISLIKES ',eval.evaluate_model(YTest,prediction,'regression',['mse','mae','r2']))
+##################################################################
+documents=dataPreProcessing.dataLoader('US','tags')
+prediction,Ytest=models.tfidfRegression(documents,viewsLog, 20000)
+print('TFIDF : TAGS ',eval.evaluate_model(Ytest,prediction,'regression',['mse','mae','r2']))
+##################################################################
+documents=dataPreProcessing.dataLoader('US','description')
+prediction,Ytest=models.tfidfRegression(documents,viewsLog, 20000)
+print('TFIDF : DESCRIPTION ',eval.evaluate_model(Ytest,prediction,'regression',['mse','mae','r2']))
+##################################################################
+documents=dataPreProcessing.dataLoader('US','title')
+prediction,Ytest=models.tfidfRegression(documents,viewsLog, 20000)
+print('TFIDF : TITLE ',eval.evaluate_model(Ytest,prediction,'regression',['mse','mae','r2']))
